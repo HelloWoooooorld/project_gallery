77dac6f7a0d6cb9d4ea5075aa7809bde7e182ecf
Yaroslav <yaroslav.pikiner@chiws.com>
2021-04-28 10:19:16 +0300

    render image

--- src/app.js
+++ src/app.js
@@ -2,33 +2,44 @@ import './js/imports';
 
 const getImgUrl = 'https://boiling-refuge-66454.herokuapp.com/images';
 const geImgIdUrl = 'https://boiling-refuge-66454.herokuapp.com/images/:imageId';
-const addComment = 'https://boiling-refuge-66454.herokuapp.com/images/:imageId/comments';
+const addComment =
+	'https://boiling-refuge-66454.herokuapp.com/images/:imageId/comments';
 
 async function getData(url) {
 	const response = await fetch(url);
 	const data = await response.json();
+  console.log(data);
 	return data;
 }
 
+async function getImg() {
+	await getData(getImgUrl).then((data) => {
+		renderImg(data);
+	});
+}
 
 async function getImg() {
-    await getData(getImgUrl).then((data) => {
-      renderImg(data.people)
-    })
-  }
+	await getData(geImgIdUrl).then((data) => {
+		renderImg(data);
+	});
+}
+
+function renderImg(data) {
+  const container = document.querySelector('.block-img');
+	const res = data.map((item) => {
+      const img = document.createElement('img');
+      const box = document.createElement('div');
+      
+      box.classList.add('box-item')
+      img.setAttribute('src', item.url);
+      img.setAttribute('id', item.id);
+
+      box.append(img);
+      container.append(box);
+	});
+  return res
+}
 
 
-  function renderImg(data) {
-    const list = document.querySelector('.list-group')
-    list.innerHTML = ''
-    
-    const total = (document.getElementById(
-      'total'
-    ).textContent = `Total people in ISS: ${data.length}`)
-    const res = data.map((item) => {
-      const listItem = document.createElement('li')
-      listItem.classList.add('list-group-item')
-      listItem.innerHTML = item.name
-      list.append(listItem)
-    })
-  }
+
+getImg();
