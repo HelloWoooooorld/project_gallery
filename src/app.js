17a50cdf185f423d6d4416cbfc049f103a6c4cdf
Yaroslav <yaroslav.pikiner@chiws.com>
2021-04-28 11:15:16 +0300

    add Delegating events

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ const getImgUrl = 'https://boiling-refuge-66454.herokuapp.com/images';
 const geImgIdUrl = 'https://boiling-refuge-66454.herokuapp.com/images/:imageId';
 const addComment =
 	'https://boiling-refuge-66454.herokuapp.com/images/:imageId/comments';
+const popup = document.querySelector('.popup');
+const blockImg = document.querySelector('.block-img');
 
 async function getData(url) {
 	const response = await fetch(url);
 	const data = await response.json();
-  console.log(data);
+	console.log(data);
 	return data;
 }
 
@@ -18,28 +20,37 @@ async function getImg() {
 	});
 }
 
-async function getImg() {
-	await getData(geImgIdUrl).then((data) => {
-		renderImg(data);
-	});
-}
-
 function renderImg(data) {
-  const container = document.querySelector('.block-img');
+	const container = document.querySelector('.block-img');
 	const res = data.map((item) => {
-      const img = document.createElement('img');
-      const box = document.createElement('div');
-      
-      box.classList.add('box-item')
-      img.setAttribute('src', item.url);
-      img.setAttribute('id', item.id);
+		const img = document.createElement('img');
+		const box = document.createElement('div');
 
-      box.append(img);
-      container.append(box);
+		box.classList.add('box-item');
+		img.setAttribute('src', item.url);
+		img.setAttribute('id', item.id);
+
+		box.append(img);
+		container.append(box);
 	});
-  return res
+	return res;
 }
 
+function show() {
+	popup.style.display = 'flex';
+}
 
+function hide() {
+	popup.style.display = 'none';
+}
 
 getImg();
+
+blockImg.onclick = function (event) {
+	let target = event.target; // где был клик?
+
+	if (target.tagName != 'IMG') return;
+	show(); // не на TD? тогда не интересует
+};
+
+document.querySelector('.popup__btn--close').addEventListener('click', hide);
