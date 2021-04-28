cecdea831fcb1a4f1420f8fe2a53ba9bff77cd5a
Yaroslav <yaroslav.pikiner@chiws.com>
2021-04-28 16:50:23 +0300

    fix timestamp converter

--- src/app.js
+++ src/app.js
@@ -1,24 +1,21 @@
 import './js/imports';
 
 const Gallery = (function () {
-	const getImgUrl = 'https://boiling-refuge-66454.herokuapp.com/images';
-	const geImgIdUrl = 'https://boiling-refuge-66454.herokuapp.com/images/';
-	const addComment =
-		'https://boiling-refuge-66454.herokuapp.com/images/:imageId/comments';
-	const popup = document.querySelector('.popup');
+  const getImgUrl = 'https://boiling-refuge-66454.herokuapp.com/images';
 
-	return {
+  const popup = document.querySelector('.popup');
 
+  return {
     getData: async function (url) {
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data);
       return data;
     },
 
     getImgFromId: async function (id) {
-      await this.getData(`${geImgIdUrl}${id}`).then((data) => {
+      await this.getData(`${getImgUrl}/${id}`).then((data) => {
         this.renderComment(data);
+        console.log(data);
       });
     },
 
@@ -27,112 +24,120 @@ const Gallery = (function () {
         this.renderImg(data);
       });
     },
+    // *****************
+    submitComment: async function (data) {
+      let response = await fetch(`${getImgUrl}/${data.imgId}/comments`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(data)
+      });
+    },
 
-		init: function () {
-			this.getImg();
-		},
+    getComment: function () {
+      const imgId = document.querySelector('.popup__img').id;
+      const name = document.getElementById('name').value;
+      const comment = document.getElementById('comment').value;
 
-		show: function (id) {
-			this.getImgFromId(id);
-		},
+      let data = {
+        imgId,
+        name,
+        comment,
+        date: parseInt((new Date().getTime() / 1000).toFixed(0)),
+      };
 
-		hide: function () {
-			popup.style.display = 'none';
+      this.submitComment(data);
+    },
+
+
+    init: function () {
+      this.getImg();
+    },
+
+    show: function (id) {
+      this.getImgFromId(id);
+    },
+
+    hide: function () {
+      popup.style.display = 'none';
       document.querySelector('.popup__img').remove();
-			document.querySelector('.comment__item').remove();
-		},
+      document.querySelector('.comment__item').remove();
+    },
 
-		renderComment: function (item) {
-			popup.style.display = 'flex';
-			const popupForm = document.querySelector('.popup__form');
-			const popupComment = document.querySelector('.popup__comment');
-			const popupImg = document.createElement('img');
+    renderComment: function (item) {
+      popup.style.display = 'flex';
 
-			popupImg.classList.add('popup__img');
-			popupImg.setAttribute('src', item.url);
-			popupImg.setAttribute('id', item.id);
-			popupForm.insertAdjacentElement('afterbegin', popupImg);
+      const popupForm = document.querySelector('.popup__form');
+      const popupComment = document.querySelector('.popup__comment');
+      const popupImg = document.createElement('img');
 
-			item.comments.map((com) => {
-				const commentItem = document.createElement('div');
-				const commentTime = document.createElement('span');
-				const commentUser = document.createElement('span');
+      popupImg.classList.add('popup__img');
+      popupImg.setAttribute('src', item.url);
+      popupImg.setAttribute('id', item.id);
+      popupForm.insertAdjacentElement('afterbegin', popupImg);
 
-				commentItem.classList.add('comment__item');
-				commentTime.classList.add('comment__item-time');
-				commentUser.classList.add('comment__item-comment');
+      item.comments.map((com) => {
+        const commentItem = document.createElement('div');
+        const commentTime = document.createElement('span');
+        const commentUser = document.createElement('span');
 
-				commentItem.setAttribute('id', com.id);
-				commentTime.textContent = this.showCurrentTime(com.date);
-				commentUser.textContent = com.text;
+        commentItem.classList.add('comment__item');
+        commentTime.classList.add('comment__item-time');
+        commentUser.classList.add('comment__item-comment');
 
-				commentItem.append(commentTime);
-				commentItem.append(commentUser);
-				popupComment.append(commentItem);
-			});
-		},
+        commentItem.setAttribute('id', com.id);
+        commentTime.textContent = this.showCurrentTime(com.date);
+        commentUser.textContent = com.text;
 
-		renderImg: function (data) {
-			const container = document.querySelector('.block-img');
-			const res = data.map((item) => {
-				const img = document.createElement('img');
-				const box = document.createElement('div');
+        commentItem.append(commentTime);
+        commentItem.append(commentUser);
+        popupComment.append(commentItem);
+      });
+    },
 
-				box.classList.add('box-item');
-				img.setAttribute('src', item.url);
-				img.setAttribute('id', item.id);
+    renderImg: function (data) {
+      const container = document.querySelector('.block-img');
+      const res = data.map((item) => {
+        const img = document.createElement('img');
+        const box = document.createElement('div');
 
-				box.append(img);
-				container.append(box);
-			});
-			return res;
-		},
+        box.classList.add('box-item');
+        img.setAttribute('src', item.url);
+        img.setAttribute('id', item.id);
 
-		showCurrentTime: function (UNIX_timestamp) {
-			let timestamp = new Date(UNIX_timestamp * 1000);
-			let months = [
-				'Jan',
-				'Feb',
-				'Mar',
-				'Apr',
-				'May',
-				'Jun',
-				'Jul',
-				'Aug',
-				'Sep',
-				'Oct',
-				'Nov',
-				'Dec',
-			];
-			let dayName = [
-				'Monday',
-				'Thusday',
-				'Wednesday',
-				'Thursday',
-				'Friday',
-				'Saturday',
-				'Sunday',
-			];
-			let year = timestamp.getFullYear();
-			let month = months[timestamp.getMonth()];
-			let date = timestamp.getDate();
-			let day = dayName[timestamp.getDay()];
-			let hour = timestamp.getHours();
-			let min = timestamp.getMinutes();
-			let time = `${hour}:${min}, ${day}, ${date} ${month} ${year}`;
+        box.append(img);
+        container.append(box);
+      });
+      return res;
+    },
 
-			return time;
-		},
-	};
+    showCurrentTime: function (UNIX_timestamp) {
+      const a = new Date(UNIX_timestamp);
+      const year = a.getFullYear();
+      const month = a.getMonth() + 1;
+      const date = a.getDate();
+      const time = `${date}.${month}.${year}`;
+      return time;
+    },
+  };
 })();
 
 Gallery.init();
 
-
 document.querySelector('.block-img').addEventListener('click', (event) => {
-	let target = event.target; // где был клик?
-	if (target.tagName != 'IMG') return;
-	Gallery.show(target.id);
+  let target = event.target; // где был клик?
+  if (target.tagName != 'IMG') return;
+  Gallery.show(target.id);
 });
 
-document.querySelector('.popup__btn--close').addEventListener('click', Gallery.hide);
+document
+  .querySelector('.popup__btn--close')
+  .addEventListener('click', Gallery.hide);
+
+document
+  .querySelector('.form__btn')
+  .addEventListener('click', (e) => {
+    e.preventDefault();
+    Gallery.getComment();
+  });
